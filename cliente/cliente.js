4298b4753040dc46349e6d6739088f7ecfbf3f40
Weliton Marcos Pereira Lima <wlima@asert.com.br>
2017-03-27 03:13:55 +0000

    Update cliente.js

--- cliente/cliente.js
+++ cliente/cliente.js
@@ -1,23 +1,3 @@
-//var net = require('net');
-//var irc = require('irc');
-//var Client = net.createConnection;
-//var client = Client();
-
-
-//var net = require('net');
-//var Client = net.createConnection;
-//var client = Client({port: 6667, localAddress: '', localPort: 51000});
-
-//client.on('connect', function() {
-//  var id = this.localAddress + ': ' + this.localPort;
-//  console.log('Client connected', id);
-//});
-
-
-//socket.connect(options[, connectListener])
-//var socket=net.socket();
-//socket.connect()
-
 
 const net = require('net');
 const client = net.connect({port: 6667}, () => {
