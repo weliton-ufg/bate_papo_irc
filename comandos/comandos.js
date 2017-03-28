c6bdbd69d66c6ceea54d32f45cff4ab0a94622f7
wlima <wlima@asert.com>
2017-03-28 01:58:03 -0300

    Refatorando código

---
+++ comandos/comandos.js
@@ -0,0 +1,33 @@
+// comandos.js
+
+//FUNÇÃO NICK
+exports.NICK = function (args, socket, nicks,retorno) {
+    var flagMudandoNick = false;
+    var nomeTemporario;
+    if (!args[1]) {
+        socket.write("ERRO:comando NINCK incompleto\n");
+        return;
+    } else if (nicks[args[1]]) {//VERIFICAR SE O NICK JA EXISTE
+        console.log(nicks);
+        socket.write("ERRO: Nickname Já existe\n");
+        return;
+    }
+    else {
+        if (socket.nick) {
+            //CASO O USÚARIO JÁ TENHA NICK FAÇO A SUBSTITUIÇÃO
+            nomeTemporario = socket.nick;
+            delete nicks[socket.nick];
+            flagMudandoNick = true;
+            retorno.push(flagMudandoNick);
+            retorno.push(nomeTemporario); 
+        }
+        nicks[args[1]] = socket.name;
+        socket.nick = args[1];
+    }
+    return retorno;
+};
+
+//FUNÇÃO USER
+exports.USER = function () {
+    //  return ;
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // Buscando biblioteca TCP
 var net = require('net');
+var comandos = require('../comandos/comandos.js');
 var clients = [];
-var nicks={};
+var nicks = {};
 var server = net.createServer(function (socket) {
 
 	socket.name = socket.remoteAddress + ":" + socket.remotePort
@@ -9,65 +10,62 @@ var server = net.createServer(function (socket) {
 	socket.write("Bem vindo " + socket.name + "\n");
 	console.log('cliente conectado');
 
+	broadcast(socket.name + " Entrou no chat\n", socket);
+	console.log(socket.name);
+
 	//EVENTO ONDE O A CONEXÃO E PERDIDA
 	socket.on('end', function () {
-	console.log('cliente desconectado');
+		console.log('cliente desconectado');
 
 	});
 	socket.on('data', function (data) {
 		// analisar a mensagem
 		analisarComando(data);
+
 	});
 
 	function analisarComando(data) {
-	var mensagem = String(data).trim();
-	// o método split quebra a mensagem em partes separadas p/ espaço em branco 
-	//dessa forma arg recebe apenas o camando
-	var args = mensagem.split(" "); 
-	if ( args[0] == "NICK" )nick(args);  // se o primeiro argumento for NICK
-	else if ( args[0] == "USER")user(args);
-	else if ( args[0] == "JOIN")join(args);
-	else socket.write("ERRO: comando inexistente\n");
+		var mensagem = String(data).trim();
+		// o método split quebra a mensagem em partes separadas p/ espaço em branco 
+		//dessa forma arg recebe apenas o camando
+		var args = mensagem.split(" ");
+		if (args[0] == "NICK") nick(args); // se o primeiro argumento for NICK
+		else if (args[0] == "USER") user(args);
+		else if (args[0] == "JOIN") join(args);
+		else socket.write("ERRO: comando inexistente\n");
 	}
 
 	// Envia mensagem a todos os clients
 	function broadcast(message, sender) {
-	clients.forEach(function (client) {
-	  // Don't want to send it to sender
-	  if (client === sender) return;
-	  client.write(message);
-	});
-	// Log it to the server output too
-	process.stdout.write(message)
+		clients.forEach(function (client) {
+			// Don't want to send it to sender
+			if (client === sender) return;
+			client.write(message);
+		});
+		// Log it to the server output too
+		process.stdout.write(message)
 	}
 
-	function join(args){
+	function nick(args) {
+		var retorno = [];
+		comandos.NICK(args, socket, nicks, retorno);
+		if (retorno[0]) {
+			broadcast(retorno[1] + " Alterou seu Nick para " + socket.nick + "\n", socket);
+			socket.write("Nick alterado com sussesso\n");
+			return;
+		} if (retorno[0]) {
+			socket.write("Nick atribuido com sussesso\n");
+		}
 
 	}
-	function nick(args){
-		if(!args[1]){
-			socket.write("ERRO:comando NINCK incompleto\n");
-			return;
-		}else if(nicks[args[1]]){//VERIFICAR SE O NICK JA EXISTE
-			socket.write("ERRO: Nickname Já existe\n");
-			return;
-		}
-		else{
-			if(socket.nick){
-				delete nicks[socket.nick];
-			}
-			nicks[args[1]]=socket.name;
-			socket.nick=args[1];
-			console.log(nicks);
-			
-		}
-		broadcast(socket.nick + " Entrou no chat\n", socket);
-		socket.write("Comando " + args + " executado com sussesso\n");
+	function join(args) {
+
 	}
-	function user(args){
+
+	function user(args) {
 
 	}
 
 }).listen(6667);
-	// Put a friendly message on the terminal of the server.
+// Put a friendly message on the terminal of the server.
 console.log("servidor rodando na porta 6667\n");
